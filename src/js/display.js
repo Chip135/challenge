9f99cfb2dfed428ad4e863c4579a178b21e2b761
Chip135 <ethandchipman@gmail.com>
2023-07-07 20:42:09 -0600

    multiple pages

--- src/js/display.js
+++ src/js/display.js
@@ -1,12 +1,12 @@
 import { getLocalStorage } from "./utils.mjs";
-import { playerWins, cpuWins } from "./functionality.js";
+import { playerWins, cpuWins, draw } from "./functionality.js";
 
 
 export async function CardTable() {
     const CpuCards = getLocalStorage("Cpu-Hand");
     //console.log(CpuCards);
     if (!CpuCards) {
-        const cardItems = "Computer hand is empty";
+        const cardItems = "You WIN!!";
         document.querySelector(".cpu-table").innerHTML = cardItems;
     } else {
         const cardItems = CpuCards.cards.map((item) => cpuTemplate(item));
@@ -15,7 +15,7 @@ export async function CardTable() {
     const playerCards = getLocalStorage("Player-Hand");
     //console.log(playerCards);
     if (!playerCards) {
-        const cardItems = "Your hand is empty";
+        const cardItems = "Better luck next time.";
         document.querySelector(".player-table").innerHTML = cardItems;
     } else {
         const cardItems = playerCards.cards.map((item) => playerTemplate(item));
@@ -25,11 +25,21 @@ export async function CardTable() {
     const curValPlay = playerCards.cards[0].value;
     if (curValCpu > curValPlay){
         cpuWins();
-        //setTimeout(location.reload(), 50000);
+        setTimeout(() => {
+            window.location.reload();
+        }, 3000);
     }
     if (curValPlay > curValCpu){
         playerWins();
-        //setTimeout(location.reload(), 50000);
+        setTimeout(() => {
+            window.location.reload();
+        }, 3000);
+    }
+    if (curValCpu == curValPlay){
+        setTimeout(draw, 1000);
+        setTimeout(() => {
+            window.location.reload();
+        }, 6000);
     }
 };
 
--- src/js/functionality.js
+++ src/js/functionality.js
@@ -4,6 +4,7 @@ import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 export function playerWins (){
     const cpuCards = getLocalStorage("Cpu-Hand");
     const playCards = getLocalStorage("Player-Hand");
+    document.querySelector(".winner").innerText = "You Win the Hand"
 
     const losCard = cpuCards.cards[0];
     const winCard = playCards.cards[0];
@@ -14,12 +15,13 @@ export function playerWins (){
 
     setLocalStorage("Player-Hand", playCards);
     setLocalStorage("Cpu-Hand", cpuCards);
-   
+
 }
 
 export function cpuWins (){
     const cpuCards = getLocalStorage("Cpu-Hand");
     const playCards = getLocalStorage("Player-Hand");
+    document.querySelector(".winner").innerText = "Computer Wins the Hand"
 
     const losCard = playCards.cards[0];
     const winCard = cpuCards.cards[0];
@@ -30,5 +32,96 @@ export function cpuWins (){
 
     setLocalStorage("Player-Hand", playCards);
     setLocalStorage("Cpu-Hand", cpuCards);
+
+}
+
+export function playerWinsDraw (){
+    const cpuCards = getLocalStorage("Cpu-Hand");
+    const playCards = getLocalStorage("Player-Hand");
+
+    const losCard = cpuCards.cards[0];
+    const winCard = playCards.cards[0];
+    const losDrawCard = cpuCards.cards[1];
+    const winDrawCard = playCards.cards[1];
+    console.log(losDrawCard);
+    console.log(winDrawCard);
+    playCards.cards.push(losCard);
+    playCards.cards.push(winCard);
+    playCards.cards.push(losDrawCard);
+    playCards.cards.push(winDrawCard);
+    cpuCards.cards.splice(0, 2);
+    playCards.cards.splice(0, 2);
+
+
+    setLocalStorage("Player-Hand", playCards);
+    setLocalStorage("Cpu-Hand", cpuCards);
    
+}
+
+export function cpuWinsDraw (){
+    const cpuCards = getLocalStorage("Cpu-Hand");
+    const playCards = getLocalStorage("Player-Hand");
+    document.querySelector(".winner").innerText = "Computer Wins Hand"
+
+    const losCard = playCards.cards[0];
+    const winCard = cpuCards.cards[0];
+    const losDrawCard = playCards.cards[1];
+    const winDrawCard = cpuCards.cards[1];
+    console.log(losDrawCard);
+    console.log(winDrawCard);
+    cpuCards.cards.push(losCard);
+    cpuCards.cards.push(winCard);
+    cpuCards.cards.push(losDrawCard);
+    cpuCards.cards.push(winDrawCard);
+    playCards.cards.splice(0, 2);
+    cpuCards.cards.splice(0, 2);
+
+    setLocalStorage("Player-Hand", playCards);
+    setLocalStorage("Cpu-Hand", cpuCards);
+   
+}
+
+export function draw (){
+    const cpuCards = getLocalStorage("Cpu-Hand");
+    const playCards = getLocalStorage("Player-Hand");
+
+    const playerItems = playCards.cards.map((item) => cpuWarTemplate(item));
+    document.querySelector(".war-player").innerHTML = playerItems[1];
+
+    const cpuItems = cpuCards.cards.map((item) => playerWarTemplate(item));
+    document.querySelector(".war-cpu").innerHTML = cpuItems[1];
+
+    const curValCpu = cpuCards.cards[1].value;
+    const curValPlay = playCards.cards[1].value;
+    if (curValCpu > curValPlay){
+        cpuWinsDraw();
+    }
+    if (curValPlay > curValCpu){
+        playerWinsDraw();
+    }
+    if (curValCpu == curValPlay){
+        draw();
+    }
+}
+
+function cpuWarTemplate(card){
+
+    const newCard = `<li class="war-cards">
+    <img class="card-image"
+        src="${card.image}"
+        alt="${card.value} of ${card.suit}">
+    </li>`;
+    
+    return newCard;
+}
+
+function playerWarTemplate(card){
+
+    const newCard = `<li class="war-cards">
+    <img class="card-image"
+        src="${card.image}"
+        alt="${card.value} of ${card.suit}">
+    </li>`;
+    
+    return newCard;
 }
